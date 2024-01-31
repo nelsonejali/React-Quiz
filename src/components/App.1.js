bfc823e69b5d15c4e8686b7a627c27cd2eff5352
nelson ejali <ejali267@gmail.com>
2024-01-31 11:04:55 +0100

    add quiz timer

---
+++ src/components/App.1.js
@@ -0,0 +1,12 @@
+import Header from './Header';
+import Main from './main';
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+
+      <Main />
+    </div>
+  );
+}
